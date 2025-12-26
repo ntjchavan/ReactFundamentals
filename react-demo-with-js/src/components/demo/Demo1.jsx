56d3464b1f5cb31ccb40714587b65a8a4d72fe57
ntjchavan <ntjchavan@gmail.com>
2025-12-26 21:30:07 +0530

    if else and dropdown demo

--- react-demo-with-js/src/components/demo/Demo1.jsx
+++ react-demo-with-js/src/components/demo/Demo1.jsx
@@ -3,19 +3,35 @@ import style from './Demo1.module.css'
 
 const Demo1 = () => {
     const product = [
-        { id: 1, name: "Apple", price: 210 },
-        { id: 2, name: "Banana", price: 90 },
-        { id: 3, name: "Pomegranite", price: 170 },
-        { id: 4, name: "Gauva", price: 130 },
+        { id: 1, name: "Apple" },
+        { id: 2, name: "Banana" },
+        { id: 3, name: "Pomegranite" },
+        { id: 4, name: "Gauva" },
     ]
 
     const [isShow, setIsShow] = useState(true);
+    const [selectedValue, setSelectedValue] = useState(0);
+    const [selectedFruit, setSelectedFruit] = useState("");
+
+    const handleFruitChange = (event) => {
+        const value = event.target.value;
+        setSelectedValue(value);
+    }
+
+    function SelectedFruit() {
+        if(selectedValue) {
+            console.log(selectedValue, product.find(x => x.id === Number(selectedValue)));
+            setSelectedFruit(product.find(x => x.id === Number(selectedValue)).name);
+        }
+        console.log('fruits')
+    }
 
     return (
         <>
             <h2>Hello, from Demo 1 component</h2>
             <hr />
             <div className={style.btnGroup}>
+                <h2>Conditional operator</h2>
                 <button className={style.btnsuccess} onClick={() => setIsShow(prev => !prev)}>
                     {isShow ? "Hide" : "Show"}
                 </button>
@@ -28,6 +44,33 @@ const Demo1 = () => {
                     : null
             }
             <hr />
+            <div className={style.btnGroup}>
+                <h2 className={style.newline}>Button Click Event</h2>
+                <select value={selectedValue} onChange={handleFruitChange}>
+                    <option value="">--- select a option ---</option>
+                    {
+                        product.map((item) => (
+                            <option key={item.id} value={item.id}>{item.name}</option>
+                        ))
+                    }
+                </select>
+                <button className={style.btnfruit} onClick={SelectedFruit}>Get Fruid Name</button>
+                <h3>Select Fruid Name: {selectedFruit}</h3>
+            </div>
+            <hr />
+            <h2>Display Product List</h2>
+            <div>
+                <ul>
+                    {
+                        product.map((item) => (
+                            <li key={item.id}>
+                                <h3>{item.id}</h3>
+                                <h3>{item.name}</h3>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </>
     )
 }
